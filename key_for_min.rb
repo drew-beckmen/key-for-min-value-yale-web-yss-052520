5efd180fb1e3a5357d2b5a165f54b0c73952911d
Drew Beckmen <drew.beckmen@yale.edu>
2020-05-14 16:46:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,9 @@ def find_key(hash)
   min_key = hash.first[0]
   min_value = hash.first[1]
   hash.each do |k, v|
-
+    if v < min_value
+        min_key = k
+        min_value = v
   end
   min_key
 end

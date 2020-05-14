1a5751177981df10681d9fc3654cd556fe1f3a12
Drew Beckmen <drew.beckmen@yale.edu>
2020-05-14 16:39:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,10 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+def find_key(hash)
+  
+end
+
 def key_for_min_value(name_hash)
   name_hash.empty? ? nil : find_key(name_hash)
 end
